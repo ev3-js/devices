c5d9ef7c72983e13155efbdf3e9e3bfeaf5cf996
Daniel <danleavitt0@gmail.com>
2015-12-08 14:37:15 -0800

    fix path

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function createMotor (basename) {
  */
 
 function portToPath () {
-  var motorPath = '/sys/class/tacho-motor'
+  var motorPath = '/sys/class/tacho-motor/'
   return fs.readdirSync(motorPath).reduce(function (obj, file) {
     var portName = fs.readFileSync(motorPath + file + '/port_name', 'utf-8')
     obj[portName] = file
