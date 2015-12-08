d85b253d8d61a6bd61735a1278e78ad894b362c0
Josh Taylor <joshrtay@gmail.com>
2015-12-07 23:27:20 -0800

    get rid of the use of this; add comments

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
+/**
+ * Imports
+ */
+
 var Motor = require('ev3-js-motor')
-var basePath = '/sys/class/tacho-motor/'
-var motors = {
+var map = require('@micro-js/map-obj')
+var path = require('path')
+
+/**
+ * Vars
+ */
+
+var motors = map(createMotor, {
   a: 'motor0',
   b: 'motor1',
   c: 'motor2',
   d: 'motor3'
-}
+})
+
+/**
+ * Expose devices
+ */
+
+module.exports = devices
+
+/**
+ * Get a device
+ * @param  {String} port
+ * @return {Device}
+ */
 
 function devices (port) {
-  this.path = basePath + motors[port.toLowerCase()]
-  return new Motor(this.path)
+  return motors[port.toLowerCase()]
+}
+
+/**
+ * Create a motor
+ * @param  {String} basename
+ * @return {Motor}
+ */
+
+function createMotor(basename) {
+  return new Motor(path.join('/sys/class/tacho-motor', basename))
 }
 
 console.log(devices('a'))
-
-exports.module = devices
